37c8254161ceafbd8ae0b547335008bff99aed50
kuruczbotond <kurucz.laszlo.botond@diak.szbi-pg.hu>
2025-03-21 13:10:07 +0100

    tablazatok megjelenítése

---
+++ kiralyok.py
@@ -0,0 +1,10 @@
+from mysql.connector import (connection)
+
+cnx = connection.MySQLConnection(user='root',
+                                 host='127.0.0.1',
+                                 database='kiralyok')
+cursor = cnx.cursor()
+cursor.execute("SHOW TABLES")
+for table in cursor:
+    print(table)
+cnx.close()
